ca5a9ecda3a09499599b58840cb3a549f31a78d7
ManavPatel25 <155817350+ManavPatel25@users.noreply.github.com>
2026-02-03 22:26:22 +0530

    Delete services directory

--- services/data_fetcher.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-def fetch_stock_data(symbol, period="6mo"):
-    data = yf.download(symbol, period=period)
-
-    if data is None or len(data) < 60:
-        raise ValueError("Not enough data for prediction")
-
-    return data[['Close']]
