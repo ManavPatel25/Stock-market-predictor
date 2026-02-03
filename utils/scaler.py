280b426464e875e31173f2154af731f194533b0f
ManavPatel25 <155817350+ManavPatel25@users.noreply.github.com>
2026-02-03 22:26:34 +0530

    Delete utils directory

--- utils/scaler.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-
-def scale_data(data):
-    scaler = MinMaxScaler()
-    scaled_data = scaler.fit_transform(data)
-
-    return scaled_data, scaler
